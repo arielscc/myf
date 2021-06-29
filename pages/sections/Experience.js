a9b92bcbf12f39b7e8d559ada47bc3613246418d
ArielScc <lf0ariel@gmail.com>
2021-06-29 19:37:14 -0400

    feat: Responsividad Experiencia

--- pages/sections/Experience.js
+++ pages/sections/Experience.js
@@ -1,69 +1,57 @@
+import React from 'react';
 import {
-  Heading,
   Box,
-  chakra,
   Text,
   Accordion,
   AccordionItem,
   AccordionButton,
   AccordionPanel,
   AccordionIcon,
-  useColorModeValue,
 } from '@chakra-ui/react';
-import React from 'react';
+import Container from '../components/Container';
 
-const Experience = () => {
-  return (
-    <Box w="5xl" mx="auto" my="20">
-      <Heading fontFamily="jet" my="5">
-        <chakra.span color="tomato" fontSize="2xl" fontWeight="900">
-          04.{' '}
-        </chakra.span>
-        Experiencia
-      </Heading>
-      <Accordion defaultIndex={[0]} allowMultiple mt="10">
-        <AccordionItem>
-          <AccordionButton>
-            <Text
-              as="h4"
-              flex="1"
-              textAlign="left"
-              fontFamily="jet"
-              color="blue.600"
-              fontWeight="700"
-            >
-              Facultad de ciencias puras y naturales
-            </Text>
-            <AccordionIcon />
-          </AccordionButton>
+const Experience = () => (
+  <Container title="Experience" index={4}>
+    <Accordion defaultIndex={[0]} allowMultiple mt="10">
+      <AccordionItem>
+        <AccordionButton>
+          <Text
+            as="h4"
+            flex="1"
+            textAlign="left"
+            fontFamily="jet"
+            color="blue.600"
+            fontWeight="700"
+          >
+            Facultad de ciencias puras y naturales
+          </Text>
+          <AccordionIcon />
+        </AccordionButton>
 
-          <AccordionPanel pb={4}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
-          </AccordionPanel>
-        </AccordionItem>
+        <AccordionPanel pb={4} textAlign={{ base: 'justify', md: 'left' }}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </AccordionPanel>
+      </AccordionItem>
 
-        <AccordionItem>
-          <h2>
-            <AccordionButton>
-              <Box flex="1" textAlign="left">
-                Section 2 title
-              </Box>
-              <AccordionIcon />
-            </AccordionButton>
-          </h2>
-          <AccordionPanel pb={4}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
-          </AccordionPanel>
-        </AccordionItem>
-      </Accordion>
-    </Box>
-  );
-};
+      <AccordionItem>
+        <AccordionButton>
+          <Box flex="1" textAlign="left">
+            Section 2 title
+          </Box>
+          <AccordionIcon />
+        </AccordionButton>
+        <AccordionPanel pb={4}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </AccordionPanel>
+      </AccordionItem>
+    </Accordion>
+  </Container>
+);
 
 export default Experience;
