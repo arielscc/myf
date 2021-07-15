2994a872c06cc1f004e5b740f72be8113c81752f
ArielScc <lf0ariel@gmail.com>
2021-07-15 19:59:10 -0400

    Add: Agregado fotografia

--- sections/About.js
+++ sections/About.js
@@ -1,7 +1,7 @@
-import { Flex, Text } from '@chakra-ui/react';
+import { Box, Flex, Text, useColorModeValue } from '@chakra-ui/react';
 import React, { useContext } from 'react';
 import Image from 'next/image';
-import profile from '../public/assets/profile.svg';
+import profile from '../public/assets/profile.jpg';
 import Container from '../components/Container';
 import AppContext from '../context/context';
 
@@ -17,7 +17,15 @@ const About = () => {
         alignItems="center"
         gridGap="5"
       >
-        <Image src={profile} alt="profilees" width={300} height={300} />
+        <Box
+          border="2px"
+          borderColor={useColorModeValue('gray.500', 'gray.100')}
+          w="xs"
+          rounded="3xl"
+          overflow="hidden"
+        >
+          <Image src={profile} alt="profilees" layout="responsive" />
+        </Box>
         <Text textAlign="justify" w={{ base: 'full', lg: 'lg' }}>
           {description}
         </Text>
