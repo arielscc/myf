a8d8073cfc2679c68c51bad713c9fdddb4028502
Ariel Chura <ariel.chura.c@gmail.com>
2023-08-16 16:10:03 -0400

    feat: update hero & education

--- sections/Hero.js
+++ sections/Hero.js
@@ -1,24 +1,15 @@
-import { Heading, Text, chakra, Button, Box, Flex } from '@chakra-ui/react';
+import { Box, Button, Flex, Heading, Text, chakra } from '@chakra-ui/react';
 import React, { useContext } from 'react';
 import { FaDownload } from 'react-icons/fa';
-import Image from 'next/image';
-import heroimage from '../public/assets/hero.svg';
 import AppContext from '../context/context';
 
 const Hero = ({ referencie }) => {
   const { hero } = useContext(AppContext);
-  const { greet, button, prof, title } = hero;
+  const { greet, button, prof, title, description } = hero;
   return (
     <chakra.header
       ref={referencie}
       h="100vh"
-      w="full"
-      bg="radial-gradient(100% 225% at 0% 0%, #DE3E3E 0%, #1b283d 100%),
-      radial-gradient(100% 225% at 100% 0%, #ff6b00 0%, #ff0000 100%),
-      linear-gradient(181deg, #CE63B7 0%, #ED6283 100%),
-      radial-gradient(100% 120% at 75% 0%, #A74600 0%, #000000 100%),
-      linear-gradient(305deg, #004596 0%, #150093 50%)"
-      bgBlendMode="overlay, color-dodge, color-burn, color-dodge, normal"
       transition=".8s"
       id={title}
     >
@@ -26,11 +17,14 @@ const Hero = ({ referencie }) => {
         direction={{ base: 'column', lg: 'row' }}
         justifyContent={{ base: 'center', lg: 'space-between' }}
         h="full"
-        maxW={{ lg: '5xl', xl: '8xl' }}
+        maxW="6xl"
         m="auto"
         px={{ base: '4', lg: 'none' }}
+        align="flex-end"
+        flexWrap="wrap"
+        alignContent="flex-end"
       >
-        <Box align="flex-start" color="white" alignSelf="self-start" mt="40">
+        <Box color="white" alignSelf="self-start" my="40">
           <chakra.span fontFamily="jet" color="tomato" fontSize="lg">
             👋🏻 {greet}
           </chakra.span>
@@ -50,9 +44,16 @@ const Hero = ({ referencie }) => {
           >
             {prof}
           </Text>
+          <Text
+            maxW={['full', 'full', 'xl']}
+            textAlign={{ base: 'justify', md: 'left' }}
+            mt="5"
+          >
+            {description}
+          </Text>
           <Button
             as="a"
-            href="/assets/resume.pdf"
+            href="/assets/resume_ariel_chura.pdf"
             target="_blank"
             rel="noopener"
             leftIcon={<FaDownload />}
@@ -64,9 +65,6 @@ const Hero = ({ referencie }) => {
           >
             {button}
           </Button>
-        </Box>
-        <Box alignSelf="flex-end">
-          <Image src={heroimage} alt="Picture of the author" width={600} />
         </Box>
       </Flex>
     </chakra.header>
