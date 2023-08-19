daf5923c090ee259f145325b5355fb80085421b1
Ariel Chura <ariel.chura.c@gmail.com>
2023-08-19 01:46:11 -0400

    feat: add ColoModeScript

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,20 @@
 /* eslint-disable react/jsx-props-no-spreading */
 
-import '@fontsource/jetbrains-mono/600.css';
-import '@fontsource/montserrat/800.css';
-import '@fontsource/montserrat/600.css';
-import '@fontsource/montserrat/500.css';
-import '@fontsource/montserrat/400.css';
 import '@fontsource/euphoria-script/400.css';
+import '@fontsource/jetbrains-mono/600.css';
+import '@fontsource/montserrat/400.css';
+import '@fontsource/montserrat/500.css';
+import '@fontsource/montserrat/600.css';
+import '@fontsource/montserrat/800.css';
 
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 import React from 'react';
-import { ChakraProvider } from '@chakra-ui/react';
 import theme from '../styles/theme';
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <Component {...pageProps} />
     </ChakraProvider>
   );
